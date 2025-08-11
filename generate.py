d9cf8d7249dccf089ba8808d6941d31bc575d638
kachhy <109984629+kachhy@users.noreply.github.com>
2025-08-11 18:02:15 -0400

    Initial

---
+++ generate.py
@@ -0,0 +1,62 @@
+# Generates leaderboard data
+# Currently accepts epoch.ai benchmark data, will expand later
+
+import pandas as pd
+import sys
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("Usage: python3 generate.py <input CSV>")
+        return
+        
+    df = pd.read_csv(sys.argv[1])
+    
+    # Isolate tests into separate dataframes
+    test_results = dict()
+    for i, row in df.iterrows():
+        if test_results.get(row["task"]): # Somewhat bad assumption because of potentially flawed data: model has only one best score 
+            test_results[row["task"]][row["model"]] = row["Best score (across scorers)"] 
+        else:
+            test_results[row["task"]] = {row["model"] : row["Best score (across scorers)"]}
+
+    # Process test scores
+    test_scores = dict()
+    for test in test_results:
+        worst_model = min(test_results[test], key=test_results[test].get)
+        worst_score = test_results[test][worst_model]
+        best_model  = max(test_results[test], key=test_results[test].get)
+        best_score  = test_results[test][best_model]
+
+        # Sanity check: If best score = worst score we skip this test since it is meaningless
+        if best_score == worst_score:
+            continue
+
+        # Compute model for each score:
+        test_scores[test] = dict()
+        for model in test_results[test]:
+            # Get relative score
+            llm_score = test_results[test][model]
+            rel_score = (llm_score - worst_score) / (best_score - worst_score)
+            test_scores[test][model] = rel_score
+
+    # Get average of all tests scores
+    preproc_test_scores = dict()
+    for test in test_scores:
+        for model in test_scores[test]:
+            if preproc_test_scores.get(model):
+                preproc_test_scores[model][0] += test_scores[test][model]
+                preproc_test_scores[model][1] += 1
+            else:
+                preproc_test_scores[model] = [test_scores[test][model], 1]
+    
+    average_test_scores = dict()
+    for model in preproc_test_scores:
+        average_test_scores[model] = preproc_test_scores[model][0] / preproc_test_scores[model][1]
+    
+    result = sorted(average_test_scores, key=average_test_scores.get, reverse=True)
+
+    for model in result:
+        print(model, round(average_test_scores[model] * 100, 1))
+
+if __name__ == "__main__":
+    main()
